f9469f6d274e0399cae471b911040aa451859152
adarshm11 <amlearning2023@gmail.com>
2024-10-08 18:33:58 -0700

    Added compatibility for navigation through webpages

--- airline-tracker/src/SearchUI.js
+++ airline-tracker/src/SearchUI.js
@@ -1,9 +1,12 @@
 import { useState } from "react"
 import "./SearchUI.css"
 import { SearchBar } from "./search-ui-components/SearchBar";
+import { useNavigate } from "react-router-dom";
 
 
 export function SearchUI() {
+
+    const navigate = useNavigate();
 
     const [results, setResults] = useState([]);
     const [results2, setResults2] = useState([]);
@@ -16,7 +19,7 @@ export function SearchUI() {
                 <SearchBar setResults={setResults2} searchType="Destination..." />
             </div>
             <div className="search-bar-button">
-                <button type="button" onClick={(e) => window.confirm("Proceed?")}>GO</button>
+                <button type="button" onClick={() => navigate("/search-results")}>GO</button>
             </div>
         </div>
 
