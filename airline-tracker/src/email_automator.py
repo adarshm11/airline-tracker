e5afb3a7b46ddf9ca45e2b9145e5d71dd0045c01
adarshm11 <amlearning2023@gmail.com>
2024-10-24 11:36:26 -0700

    Modified .gitignore, email server automation

--- airline-tracker/src/email_automator.py
+++ airline-tracker/src/email_automator.py
@@ -1,32 +1,43 @@
 import getpass
 import smtplib
+import random
 
-HOST = "smtp-mail.outlook.com"
+HOST = 'smtp-mail.outlook.com'
 PORT = 587
 
-FROM_EMAIL = "airlinetracker@outlook.com"
-TO_EMAIL = "adarshmallya34@gmail.com"
-PASSWORD = getpass.getpass("Enter password: ")
+FROM_EMAIL = 'airlinetracker@outlook.com'
+TO_EMAIL = None # process from database based on request
 
-MESSAGE = """Subject: Automated Email Test
+def send_mail(FROM_EMAIL, TO_EMAIL, MESSAGE):
+    smtp = smtplib.SMTP(HOST, PORT)
 
-Hello,
+    status_code, response = smtp.ehlo()
+    print(f'[*] Echoing the server: {status_code} {response}')
 
-This email is to test that the automated email server words as intended.
+    status_code, response = smtp.starttls()
+    print(f'[*] Starting TLS connection: {status_code} {response}')
 
-Thanks,
-AM"""
+    status_code, response = smtp.login(FROM_EMAIL, PASSWORD)
+    print(f'[*] Logging in: {status_code} {response}')
 
-smtp = smtplib.SMTP(HOST, PORT)
+    smtp.sendmail(FROM_EMAIL, TO_EMAIL, MESSAGE)
+    smtp.quit()
 
-status_code, response = smtp.ehlo()
-print(f"[*] Echoing the server: {status_code} {response}")
+def notify(alert, departure, arrival):
+    # traverse through DB
+    # for each user that subscribed to flight from 'departure' and to 'arrival:
+    # send email using alert.txt template to notify them
+    pass
 
-status_code, response = smtp.starttls()
-print(f"[*] Starting TLS connection: {status_code} {response}")
+def verify(email_address):
+    random_num = random.randint(0, 999999)
+    code = int(f'{random_num:06}')
+    TO_EMAIL = email_address
+    # send email to 'email_address' using verify.txt to confirm code
+    # when authentication complete, send request to add 'email_address' to database
+    # request will handle inputting subscribed airlines
+    
+with open('password.txt', 'r') as file:
+    PASSWORD = file.readline().strip()
 
-status_code, response = smtp.login(FROM_EMAIL, PASSWORD)
-print(f"[*] Logging in: {status_code} {response}")
-
-smtp.sendmail(FROM_EMAIL, TO_EMAIL, MESSAGE)
-smtp.quit()
+MESSAGE = None # process request --> read from either verify.txt or alert.txt
