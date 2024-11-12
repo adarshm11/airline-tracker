d0f8e3f14e51883a46d3c2588020242a20f466c3
adarshm11 <amlearning2023@gmail.com>
2024-11-12 11:24:21 -0800

    Modified component to add new users to subscription database to include compatibility for automated emails, connection to Flask backend

--- airline-tracker/src/AddUserForm.js
+++ airline-tracker/src/AddUserForm.js
@@ -7,69 +7,103 @@ export function AddUserForm() {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [error, setError] = useState('');
+  const [verificationCode, setVerificationCode] = useState('');
+  const [isVerified, setIsVerified] = useState(false);
+  const [isVerificationSent, setIsVerificationSent] = useState(false);
 
   const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    if (name.trim() === ""){
-      setError("Please enter your name.")
-      return;
+    if (!isVerificationSent){
+      if (name.trim() === ""){
+        setError("Please enter your name.")
+        return;
+      }
+
+      if (!emailRegex.test(email) || email.trim() === "") {
+        setError('Please enter a valid email address.');
+        return;
+      }
+      setError('');
+
+      try {
+        const response = await axios.post('http://localhost:5001/add-user', {
+          name,
+          email,
+        });
+
+        console.log('User added:', response.data);
+        setIsVerificationSent(true);
+        setName('');
+        setEmail('');
+      } catch (error) {
+
+        if (error.response && error.response.data) {
+          setError(error.response.data.message || 'There was an error adding the user.');
+        } 
+        else {
+          setError('An unexpected error occurred. Please try again later.');
+        }
+      }
     }
-
-    if (!emailRegex.test(email) || email.trim() === "") {
-      setError('Please enter a valid email address.');
-      return;
-    }
-    setError('');
-
-    try {
-      const response = await axios.post('http://localhost:5001/api/users', {
-        name,
-        email,
-      });
-
-      console.log('User added:', response.data);
-      // Optionally, reset the form
-      setName('');
-      setEmail('');
-    } catch (error) {
-
-      if (error.response && error.response.data) {
-        setError(error.response.data.message || 'There was an error adding the user.');
-      } 
-      
-      else {
-        setError('An unexpected error occurred. Please try again later.');
+    else {
+      try {
+        const response = await axios.post("http://localhost:5001/verify-user", {
+          email,
+          code: verificationCode,
+        })
+        console.log("User verified:", response.data);
+        setIsVerified(true);
+        setError("");
+      } catch (error) {
+        if (error.response && error.response.data){
+          setError(error.response.data || "There was an error verifying the code.");
+        } else {
+          setError("An unexpected error occurred. Please try again later.");
+        }
       }
     }
   };
 
   return (
     <div>
-      <h1>Enter Your Information</h1>
+      <h1>{isVerified ? "Verify Your Email" : "Enter Your Information"}</h1>
       <form className="form-container" onSubmit={handleSubmit}>
-          <input className="name-input-field"
-            type="text"
-            placeholder="Name..."
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-          />
-
-          <input className="email-input-field"
-            type="text"
-            placeholder="Email..."
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
+        {!isVerificationSent ? (
+          <>
+            <input className="name-input-field"
+              type="text"
+              placeholder="Name..."
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+            <input className="email-input-field"
+              type="text"
+              placeholder="Email..."
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </>
+        ) : (
+          <>
+            <input 
+              className="verification-input-field"
+              type="text"
+              placeholder="Enter your verification code..."
+              value={verificationCode}
+              onChange={(e) => setVerificationCode(e.target.value)}
+            />
+          </>
+        )}
 
         {error && <div className="error-message">{error}</div>}
 
         <button 
           className="submit-button" 
           type="submit">
-            <strong>SUBMIT</strong>
+            <strong>{isVerificationSent ? "VERIFY" : "SUBMIT"}</strong>
         </button>
       </form>
     </div>
--- airline-tracker/src/App.js
+++ airline-tracker/src/App.js
@@ -3,11 +3,15 @@ import { FlightResults } from './FlightResults.js';
 import { SearchUI } from './SearchUI.js';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { AddUserForm } from './AddUserForm.js';
+import logo from './logo.png';
 
 function App() {
   return (
     <div className="App">
       <Router>
+        <nav className="nav-bar">
+          <a href="/"><img src={logo} className="logo" alt="HOME"/></a>
+        </nav>
         <Routes>
           <Route path="/" element={<SearchUI/>}/>
           <Route path="/search-results" element={<FlightResults/>}/>
